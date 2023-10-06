b3b120f411ba234a1851f50531127b4c0485bc0e
Princewill Inyang <97766398+princewilling@users.noreply.github.com>
2023-10-06 21:41:55 +0100

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,6 @@ const dbConnect = async () => {
     try {
         const connected = await mongoose.connect(
             process.env.MONGO_URI
-            // "mongodb+srv://princewillingoo:kVESlWWMvMX785tz@nodejs-ecommerce-api.6a7hskp.mongodb.net/nodejs-ecommerce-api?retryWrites=true&w=majority"
         )
         console.log(`Mongodb connected ${connected.connection.host}`);
     } catch (error) {
